c6e98b12e52e7b42148c92ab76151b773b926c50
Tony Barquera <barqueratony@gmail.com>
2024-04-16 17:26:27 -0600

    base structure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className="m-5 text-center text-3xl">Web by Tony Barquera</h1>
+      {/* Header */}
+      {/* Hero */}
+      {/* About Me */}
+      {/* Projects */}
+      {/* Footer */}
     </>
   )
 }
