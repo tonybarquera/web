3b3f6fc42b861a625f775df9710522a47bbb7376
Tony Barquera <barqueratony@gmail.com>
2024-12-25 13:55:08 -0600

    Change design in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,13 @@ function Header() {
   }
 
   return (
-    <header className="bg-primary py-8 px-10">
-      <div className="max-w-5xl mx-auto flex flex-col gap-5 md:flex-row md:justify-between items-center">
-        <h1 className="text-center text-3xl font-bold font-roboto text-primary">Tony Barquera</h1>
-      
-        { <FaBars className='md:hidden' color='white' size='28px' onClick={handleMenu} /> }
+    <header className="bg-primary py-3 px-3">
+      <div className="max-w-5xl mx-auto md:flex md:justify-between md:items-center">
+        <div className='flex justify-between items-center w-100'>
+          <h1 className="text-center text-lg font-bold font-roboto text-primary">Tony Barquera</h1>
+        
+          { <FaBars className='md:hidden' color='#E3C78F' size='28px' onClick={handleMenu} /> }
+        </div>
         
         <nav className={`flex flex-col md:flex-row gap-5 md:gap-10  ${ !activeMenu ? 'hidden' : ''} md:flex items-center transition ease-in-out delay-500`}>
           <NavLink text="About Me" reference='about' setActiveMenu={setActiveMenu} />
